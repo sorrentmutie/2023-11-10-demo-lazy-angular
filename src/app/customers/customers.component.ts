8edb4d8bf56ecdd10ad0581a7b963a056fb2e27a
Salvatore Sorrentino <salvatore.sorrentino@live.com>
2023-11-10 16:16:20 +0100

    first commit

---
+++ src/app/customers/customers.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customers',
+  template: `
+    <p>
+      customers works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class CustomersComponent {
+
+}
---
+++ src/app/orders/orders.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-orders',
+  template: `
+    <p>
+      orders works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class OrdersComponent {
+
+}
